3f786a23e285b0c8c6401430df71ec7a9f61cd4d
Usman <it.dev45@dawaateislami.net>
2024-09-05 21:41:11 +0500

    order + checkout issue fixing + add to cart product without sides

--- resources/views/pages/checkout.blade.php
+++ resources/views/pages/checkout.blade.php
@@ -17,185 +17,130 @@
                     <input type="hidden" id="stripe_key" value="{{ $stripeKey }}">
                     <form id="checkout-form" action="{{ route('checkout.process') }}" method="post">
                         @csrf
-                        <!-- <ol>
-                            <li>
-                                <h4>Contact information</h4>
+                        <div class="card shadow-0 border">
+                            <div class="p-4">
+                                <!-- Contact Details -->
+                                <h5 class="card-title mb-3">Contact Information</h5>
                                 <div class="row">
-                                    <div class="col-md-4 p-0">
-                                        <input type="text" placeholder="Name" name="name" class="form-control" required>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <input type="email" placeholder="Email" name="email" class="form-control" required>
-                                    </div>
-                                    <div class="col-md-4">
-                                        <input type="text" placeholder="Phone" name="phone" class="form-control" required>
-                                    </div>
-                                </div>
-                            </li>
-                            @if ($orderType == 'delivery')
-                                <li>
-                                    <h4>Delivery Address</h4>
-                                    <p>Enter the address where you want your order delivered.</p>
-                                    <div class="row">
-                                        <div class="col-md-12 p-0">
-                                            <input type="text" placeholder="Address" name="address" class="form-control" required>
+                                    <div class="col-12 mb-3">
+                                        <p class="mb-0">Name</p>
+                                        <div class="form-outline">
+                                            <input type="text" name="name" id="name" placeholder="Type here" class="form-control" required/>
                                         </div>
                                     </div>
-                                    <h5 class="pt-4">Select your location</h4>
-                                    <div class="row">
-                                        <input type="hidden" id="coordinates" name="coordinates" value="">
-                                        <input type="hidden" id="customer-lat" name="customer_lat">
-                                        <input type="hidden" id="customer-lng" name="customer_lng"> 
-                                        <div id="map"></div>
+                    
+                                    {{-- <div class="col-6">
+                                        <p class="mb-0">Last name</p>
+                                        <div class="form-outline">
+                                            <input type="text" id="typeText" placeholder="Type here" class="form-control" />
+                                        </div>
+                                    </div> --}}
+                    
+                                    <div class="col-6 mb-3">
+                                        <p class="mb-0">Phone</p>
+                                        <div class="form-outline">
+                                            <input type="tel" name="phone" id="phone" class="form-control" required/>
+                                        </div>
                                     </div>
-                                </li>
-                            @endif
-                            <li class="otp">
-                                <h4>Payment options</h4>
-                                <div class="option-select">
-                                    <input type="radio" id="cash" name="payment_option" value="cash" checked required>
-                                    <label for="cash"> CASH</label>
+                    
+                                    <div class="col-6 mb-3">
+                                        <p class="mb-0">Email</p>
+                                        <div class="form-outline">
+                                            <input type="email" name="email" id="email" placeholder="example@gmail.com" class="form-control" />
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="option-select">
-                                    <input type="radio" id="online" name="payment_option" value="online" required>
-                                    <label for="online"> ONLINE </label>
+                    
+                                <!-- Address Details -->
+                                @if ($orderType == 'delivery')
+                                    <hr class="my-4" />
+                                    <h5 class="card-title mb-3">Address Details</h5>
+                                    <div class="row">
+                                        <div class="col-sm-12 mb-3">
+                                            <p class="mb-0">Address</p>
+                                            <div class="form-outline">
+                                                <input type="text" id="address" name="address" placeholder="Type here" class="form-control" required />
+                                            </div>
+                                        </div>
+                        
+                                        <div class="col-sm-12 mb-3">
+                                            <p class="mb-0">Apartment, Suite, etc. (Optional)</p>
+                                            <div class="form-outline">
+                                                <input type="text" id="apartment" name="apartment" placeholder="Type here" class="form-control" />
+                                            </div>
+                                        </div>
+                        
+                                        <div class="col-sm-6 col-6 mb-3">
+                                            <p class="mb-0">City</p>
+                                            <div class="form-outline">
+                                                <input type="text" id="city" name="city" class="form-control" required />
+                                            </div>
+                                        </div>
+                        
+                                        <div class="col-sm-6 col-6 mb-3">
+                                            <p class="mb-0">Postal code</p>
+                                            <div class="form-outline">
+                                                <input type="text" id="postcode" name="postcode" class="form-control" required/>
+                                            </div>
+                                        </div>
+                        
+                                        <input type="hidden" id="latitude" name="latitude">
+                                        <input type="hidden" id="longitude" name="longitude">
+                                    </div>
+                                @endif
+
+                                <hr class="my-4" />
+                                <div class="mb-3">
+                                    <p class="mb-0">Add a note to your order</p>
+                                    <div class="form-outline">
+                                        <textarea class="form-control" id="note" name="note" rows="2"></textarea>
+                                    </div>
                                 </div>
-                            </li>
-                        </ol> -->
 
+                                <!-- Payment Detail -->
+                                <hr class="my-4" />
+                                <h5 class="card-title mb-3">Payment Option</h5>
+                                <div class="row mb-3">
+                                    <div class="col-lg-4 mb-3">
+                                        <div class="form-check h-100 border rounded-3">
+                                            <div class="p-3">
+                                                <input class="form-check-input" type="radio" name="payment_option" id="cash" value="cash" checked required/>
+                                                <label class="form-check-label" for="cash">
+                                                    Cash <br />
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-4 mb-3">
+                                        <div class="form-check h-100 border rounded-3">
+                                            <div class="p-3">
+                                                <input class="form-check-input" type="radio" name="payment_option" id="online" value="online" required/>
+                                                <label class="form-check-label" for="online">
+                                                    Online <br />
+                                                </label>
+                                            </div>
+                                        </div>
+                                    </div>
 
-                          <!-- Checkout -->
-        <div class="card shadow-0 border">
-          <div class="p-4">
-            <h5 class="card-title mb-3">Contact Information</h5>
-            <div class="row">
-              <div class="col-6 mb-3">
-                <p class="mb-0">First name</p>
-                <div class="form-outline">
-                  <input type="text" id="typeText" placeholder="Type here" class="form-control" />
-                </div>
-              </div>
-
-              <div class="col-6">
-                <p class="mb-0">Last name</p>
-                <div class="form-outline">
-                  <input type="text" id="typeText" placeholder="Type here" class="form-control" />
-                </div>
-              </div>
-
-              <div class="col-6 mb-3">
-                <p class="mb-0">Phone</p>
-                <div class="form-outline">
-                  <input type="tel" id="typePhone" value="+48 " class="form-control" />
-                </div>
-              </div>
-
-              <div class="col-6 mb-3">
-                <p class="mb-0">Email</p>
-                <div class="form-outline">
-                  <input type="email" id="typeEmail" placeholder="example@gmail.com" class="form-control" />
-                </div>
-              </div>
-            </div>
-
-            <hr class="my-4" />
-
-            <h5 class="card-title mb-3">Payment Option</h5>
-
-            <div class="row mb-3">
-              <div class="col-lg-4 mb-3">
-                <div class="form-check h-100 border rounded-3">
-                  <div class="p-3">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked />
-                    <label class="form-check-label" for="flexRadioDefault1">
-                      Cash <br />
-                    </label>
-                  </div>
-                </div>
-              </div>
-              <div class="col-lg-4 mb-3">
-                <div class="form-check h-100 border rounded-3">
-                  <div class="p-3">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault3" />
-                    <label class="form-check-label" for="flexRadioDefault3">
-                      Online <br />
-                    </label>
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div class="row">
-              <div class="col-sm-12 mb-3">
-                <p class="mb-0">Address</p>
-                <div class="form-outline">
-                  <input type="text" id="address" name="address" placeholder="Type here" class="form-control" required />
-                </div>
-              </div>
-
-              <div class="col-sm-12 mb-3">
-                <p class="mb-0">Apartment, Suite, etc. (Optional)</p>
-                <div class="form-outline">
-                  <input type="text" id="apartment" name="apartment" placeholder="Type here" class="form-control" />
-                </div>
-              </div>
-
-              <div class="col-sm-6 col-6 mb-3">
-                <p class="mb-0">City</p>
-                <div class="form-outline">
-                  <input type="text" id="city" name="city" class="form-control" required />
-                </div>
-              </div>
-
-              <div class="col-sm-6 col-6 mb-3">
-                <p class="mb-0">Postal code</p>
-                <div class="form-outline">
-                  <input type="text" id="postcode" name="postcode" class="form-control" required/>
-                </div>
-              </div>
-
-                <input type="hidden" id="latitude" name="latitude">
-                <input type="hidden" id="longitude" name="longitude">
-            </div>
-
-            <div class="mb-3">
-              <p class="mb-0">Message to seller</p>
-              <div class="form-outline">
-                <textarea class="form-control" id="textAreaExample1" rows="2"></textarea>
-              </div>
-            </div>
-
-            <div class="float-end">
-              <button class="" style="background-color: transparent; padding: 10px 13px; border: 2px solid #e26284; border-radius: 0 20px 0 0; margin: 10px 15px; color: #e26284;">Cancel</button>
-              <button id="place-order" class="nav-top-svg text-white shadow-0 border-white">Place Order</button>
-            </div>
-          </div>
-        </div>
-        <!-- Checkout -->
-
-
-
-                        <!-- Stripe Payment Form -->
-                        <!-- <div id="stripe-form" class="container mt-4 d-none">
-                            <h4 class="mb-4">Stripe Payment</h4>
-                            <div class="form-group mb-3">
-                                <label for="card-element" class="form-label">Credit Card Information</label>
-                                <div id="card-element" class="form-control"></div>
-                                <div id="card-errors" role="alert" class="text-danger mt-2"></div>
-                            </div>
-                            <div class="form-group">
-                                <button id="submit-payment" type="button" class="btn w-100" style="color: #C36; border: 1px solid #C36">Confirm the Payment and Place Order</button>
+                                    <!-- Stripe Card Fields -->
+                                    <div id="stripe-form" class="container mt-4 d-none">
+                                        <h4 class="mb-4">Stripe Payment</h4>
+                                        <div class="form-group mb-3">
+                                            <label for="card-element" class="form-label">Credit Card Information</label>
+                                            <div id="card-element" class="form-control"></div>
+                                            <div id="card-errors" role="alert" class="text-danger mt-2"></div>
+                                        </div>
+                                        <div class="form-group">
+                                            <button id="submit-payment" type="button" class="btn w-100" style="color: #C36; border: 1px solid #C36">Confirm the Payment and Place Order</button>
+                                        </div>
+                                    </div>
+                                </div>
+                  
+                                <div class="float-end">
+                                    <button id="place-order" class="nav-top-svg text-white shadow-0 border-white">Place Order</button>
+                                </div>
                             </div>
                         </div>
-                        <div class="add-note">
-                            <label for="note">Add a note to your order</label>
-                            <textarea cols="30" rows="3" name="note" class="form-control"></textarea>
-                        </div>
-                        <hr>
-                        <div class="return mt-4">
-                            <a href="{{ route('cart.view') }}">Return to cart</a>
-                            <button id="place-order" type="submit" class="btn">Place order</button>
-                        </div> -->
                     </form>
                 </div>
             </div>
