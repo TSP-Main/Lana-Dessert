b4b1a17c6d56d5ad9e251091e4ab8c90b6dc04ac
Usman <it.dev45@dawaateislami.net>
2024-09-16 21:40:54 +0500

    print functionality + dynamic order receipt + add categories on product page

--- resources/views/pages/products.blade.php
+++ resources/views/pages/products.blade.php
@@ -16,7 +16,7 @@
     </div>
 
     <!-- Modal -->
-<div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="cartModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
         <div class=" w-100 modal-content">
             <div class="modal-header">
@@ -36,11 +36,45 @@
         </div>
     </div>
 
-
     <div class="promo text-center my-5 py-5">
     <!-- <h2 class="special">{{ isset($category) ? ucfirst($category) : '' }}</h2> -->
     <div class="container mt-5 pt-5">
     <div class="row" data-aos="fade-up" data-aos-duration="1500">
+
+        <!-- Navigation Row with Scroll Arrows -->
+        <div class="position-relative">
+            <div class="row mb-5">
+                <div class="col-12">
+                    <!-- Navigation Container -->
+                    <div style="display: flex; justify-content: center; padding-left: 20px; padding-right: 20px;">
+                        <div class="scrollable-nav" style="display: flex; overflow: auto; width: 95%; white-space: nowrap; scroll-behavior: smooth;">
+                            @foreach ($menus as $menu)
+                            <a 
+                                class="nav-link mr-3 mr-sm-n3 fw-bold text-dark"
+                                href="{{ route('menu', ['category' => $menu['attributes']['slug']]) }}"
+                                data-scroll-to="{{ $menu['attributes']['slug'] }}" 
+                                style="font-size: 20px; text-decoration: none;">
+                                {{ $menu['attributes']['name'] }}
+                            </a>
+                            @endforeach
+                        </div>
+                    </div>
+                    
+                    <button class="position-absolute top-50 start-0 translate-middle-y scroll-btn" id="scroll-left">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-left" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>
+                        </svg>
+                    </button>
+                
+                    <button class="position-absolute top-50 end-0 translate-middle-y scroll-btn" id="scroll-right">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </div>
+
         @if ($response)
             @foreach ($products as $product)
                 <!-- Adjusting column classes for different screen sizes and adding margin on mobile -->
